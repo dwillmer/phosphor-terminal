dcb45e906da14175f341060b928c4cb5782c57ec
Steven Silvester <steven.silvester@ieee.org>
2015-06-18 15:21:21 -0500

    Rebuild

--- dist/phosphor-terminal.js
+++ dist/phosphor-terminal.js
@@ -108,8 +108,8 @@ var phosphor;
                     this._term_row_height = this._dummy_term.offsetHeight / 25;
                     this._term_col_width = this._dummy_term.offsetWidth / 80;
                 }
-                var rows = Math.max(2, Math.floor(height / this._term_row_height) - 1);
-                var cols = Math.max(3, Math.floor(width / this._term_col_width) - 1);
+                var rows = Math.max(2, Math.floor(height / this._term_row_height) - 2);
+                var cols = Math.max(3, Math.floor(width / this._term_col_width) - 2);
                 rows = this._config.rows || rows;
                 cols = this._config.cols || cols;
                 this._term.resize(cols, rows);
